2b7efacb54c2bd6e426a9b3e7ebc08691cefbb66
Rian Lima <rlimamendes@proton.me>
2025-03-20 01:22:11 -0300

    frontend and backend communication

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,22 +1,19 @@
+'use client'
+
 import { Template, ImageCard }  from "@/components"
+import { Image } from '@/resources'
+import { useImageService } from '@/resources'
+import { useState } from 'react' 
 
 export default function GaleriaPage() {
+
+    const useService = useImageService();
+    const [images, setImages] = useState<Image[]>([]);
+
     return (
         <Template>
             <section className="grid grid-cols-4 gap-8">
-                <ImageCard nome="Nature" tamanho="10MB" dataUpload="18/03/2025" source="https://good-nature-blog-uploads.s3.amazonaws.com/uploads/2022/01/good-nature-homepage-hero_2-scaled.jpg"/>
-                <ImageCard nome="Nature" tamanho="10MB" dataUpload="18/03/2025" source="https://good-nature-blog-uploads.s3.amazonaws.com/uploads/2022/01/good-nature-homepage-hero_2-scaled.jpg"/>
-                <ImageCard nome="Nature" tamanho="10MB" dataUpload="18/03/2025" source="https://good-nature-blog-uploads.s3.amazonaws.com/uploads/2022/01/good-nature-homepage-hero_2-scaled.jpg"/>
-                <ImageCard nome="Nature" tamanho="10MB" dataUpload="18/03/2025" source="https://good-nature-blog-uploads.s3.amazonaws.com/uploads/2022/01/good-nature-homepage-hero_2-scaled.jpg"/>
-                <ImageCard nome="Nature" tamanho="10MB" dataUpload="18/03/2025" source="https://good-nature-blog-uploads.s3.amazonaws.com/uploads/2022/01/good-nature-homepage-hero_2-scaled.jpg"/>
-                <ImageCard nome="Nature" tamanho="10MB" dataUpload="18/03/2025" source="https://good-nature-blog-uploads.s3.amazonaws.com/uploads/2022/01/good-nature-homepage-hero_2-scaled.jpg"/>
-                <ImageCard nome="Nature" tamanho="10MB" dataUpload="18/03/2025" source="https://good-nature-blog-uploads.s3.amazonaws.com/uploads/2022/01/good-nature-homepage-hero_2-scaled.jpg"/>
-                <ImageCard nome="Nature" tamanho="10MB" dataUpload="18/03/2025" source="https://good-nature-blog-uploads.s3.amazonaws.com/uploads/2022/01/good-nature-homepage-hero_2-scaled.jpg"/>
-                <ImageCard nome="Nature" tamanho="10MB" dataUpload="18/03/2025" source="https://good-nature-blog-uploads.s3.amazonaws.com/uploads/2022/01/good-nature-homepage-hero_2-scaled.jpg"/>
-                <ImageCard nome="Nature" tamanho="10MB" dataUpload="18/03/2025" source="https://good-nature-blog-uploads.s3.amazonaws.com/uploads/2022/01/good-nature-homepage-hero_2-scaled.jpg"/>
-                <ImageCard nome="Nature" tamanho="10MB" dataUpload="18/03/2025" source="https://good-nature-blog-uploads.s3.amazonaws.com/uploads/2022/01/good-nature-homepage-hero_2-scaled.jpg"/>
-                <ImageCard nome="Nature" tamanho="10MB" dataUpload="18/03/2025" source="https://good-nature-blog-uploads.s3.amazonaws.com/uploads/2022/01/good-nature-homepage-hero_2-scaled.jpg"/>
-                <ImageCard nome="Nature" tamanho="10MB" dataUpload="18/03/2025" source="https://good-nature-blog-uploads.s3.amazonaws.com/uploads/2022/01/good-nature-homepage-hero_2-scaled.jpg"/>
+                <ImageCard nome="{nameImage}" tamanho="10MB" dataUpload="18/03/2025" source=""/>
             </section>
         </Template>
     );
---
+++ src/resources/index.ts
@@ -0,0 +1,2 @@
+export { useImageService } from '@/resources/image.service'
+export { Image } from '@/resources/image.resource'
