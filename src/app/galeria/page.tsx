66d957ed8dbc77d9d3a18da359987c19e38d090c
Rian Lima <rlimamendes@proton.me>
2025-03-20 01:40:11 -0300

    render images on frontend

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -10,10 +10,26 @@ export default function GaleriaPage() {
     const useService = useImageService();
     const [images, setImages] = useState<Image[]>([]);
 
+    async function searchAllImages() {
+        const response = await useService.buscar();
+        setImages(response);
+    }
+
+    function renderImageCard(image : Image) {
+        return (
+            <ImageCard nome={image.name} source={image.url} tamanho={image.size} dataUpload={image.uploadedAt} />
+        );
+    }
+
+    function renderImagesCard() {
+        return images.map(image => renderImageCard(image));
+    }
+
     return (
         <Template>
+            <button className="bg-gray-500 cursor-pointer" onClick={searchAllImages} >Carregar imagens</button>
             <section className="grid grid-cols-4 gap-8">
-                <ImageCard nome="{nameImage}" tamanho="10MB" dataUpload="18/03/2025" source=""/>
+                { renderImagesCard() }
             </section>
         </Template>
     );
