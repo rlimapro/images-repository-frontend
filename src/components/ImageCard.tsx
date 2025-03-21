8b524c6141e2e7196f430dd2d899e101854451ee
Rian Lima <rlimamendes@proton.me>
2025-03-20 23:56:10 -0300

    image download operation

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 interface ImageCardProps {
     nome?: string;
     tamanho?: string;
@@ -6,9 +8,15 @@ interface ImageCardProps {
 }
 
 const ImageCard = ({nome, tamanho, dataUpload, source} : ImageCardProps) => {
+
+    function download() {
+        window.open(source, '_blank');
+    }
+
     return (
         <div className="card relative bg-white rounded-md shadow-md transition-transform ease-in duration-300 transform hover:shadow-lg hover:-translate-y-2 cursor-pointer">
-            <img className="h-56 w-full object-cover rounded-t-md" src={source} alt="" />
+            <img onClick={download}
+                 className="h-56 w-full object-cover rounded-t-md" src={source} alt="" />
             <div className="card-body p-4">
                 <h5 className="text-x1 font-semibold mb-2 text-gray-600">{nome}</h5>
                 <p className="text-gray-600">{tamanho}</p>
