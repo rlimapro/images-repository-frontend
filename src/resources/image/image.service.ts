ed69059340e08bc2dc623dc675fc8ab63232ef31
Rian Lima <rlimamendes@proton.me>
2025-04-03 15:36:50 -0300

    send jwt from frontend

--- src/resources/image/image.service.ts
+++ src/resources/image/image.service.ts
@@ -1,18 +1,29 @@
 import { Image } from './image.resource'
+import { useAuth } from '@/resources'
 
 class ImageService {
     baseUrl = 'http://localhost:8080/v1/images';
+    auth = useAuth();
 
     async buscar(query : string = "", extension : string = "") : Promise<Image[]> {
+        const userSession = this.auth.getUserSession();
         const url = `${this.baseUrl}?query=${query}&extension=${extension}`
-        const response = await fetch(url);
+        const response = await fetch(url, {
+            headers: {
+                "Authorization": `Bearer ${userSession?.accessToken}`
+            }
+        });
         return await response.json();
     }
 
     async salvar(dados : FormData) : Promise<string> {
+        const userSession = this.auth.getUserSession();
         const response = await fetch(this.baseUrl, {
             method: 'POST',
-            body: dados
+            body: dados,
+            headers: {
+                "Authorization": `Bearer ${userSession?.accessToken}`
+            }
         });
         return response.headers.get('location') ?? '';
     }
