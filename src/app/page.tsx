cb004296a826a3e1b7995afcf3cfb554fa7cbda3
Rian Lima <rlimamendes@proton.me>
2025-04-06 16:53:26 -0300

    link root to galery page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+'use client'
+
+import { useAuth } from "@/resources";
+import Login from './login/page'
+import GaleriaPage from "./galeria/page";
 
 export default function Home() {
+	
+	const auth = useAuth();
+	const user = auth.getUserSession();
+
+	if(!user) {
+		return <Login/>
+	}
+	
 	return(
-		<>
-		
-		</>
+		<GaleriaPage/>
 	);
 }
