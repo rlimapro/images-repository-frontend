d8d6ed80837263048b85927bc39c4423a12365c0
Rian Lima <rlimamendes@proton.me>
2025-04-08 21:31:07 -0300

    custom not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Template } from "@/components";
+
+export default function NotFoundPage() {
+    return (
+        <Template>
+            <section>
+                <h1 className="text-gray-900 text-xl text-center">Page not found!</h1>
+            </section>
+        </Template>
+    );
+}
